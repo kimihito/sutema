29cb48276aecd9b811050f62393ea6025dea0a2d
kimihito <tatsurotamashiro@gmail.com>
2012-10-28 08:34:11 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sutema::Application.config.secret_token = 'eea43c976dfb23b94bd3eb9d56a303352d5d78fdc09041bfe326eb57aa947037d8d6dfe92a36faee0c4f25b6e824350fa3793ba434050df3399377e5c0ce1651'
